da817279e0c0b3a89bbe1f2bc8ad6e69d3058687
tina-fernan <tinaaroom@gmail.com>
2026-04-08 14:57:53 +0200

    modified cypress.config.js & add Mochawesome Report to  Project

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    setupNodeEvents(on, config) {},
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      screenshotOnRunFailure: true
+    }
+  }
+})
