64688a05d44b5b2effa70b5cfbad6eb4541ebf59
tina-fernan <tinaaroom@gmail.com>
2026-04-08 16:14:59 +0200

    updated cypress config & CI workflows

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,19 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
+    baseUrl: 'https://www.saucedemo.com',
+     env: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/reports',
       overwrite: false,
       html: true,
       json: true,
-      screenshotOnRunFailure: true
+      screenshotOnRunFailure: true,
+      allowCypressEnv: false 
     }
   }
 })
